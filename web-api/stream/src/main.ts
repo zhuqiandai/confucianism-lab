6b4bbb2e7967d480369bc9f87424200e724efdea
wangzhx <1132154046@qq.com>
2024-06-14 17:18:25 +0800

    stream

---
+++ web-api/stream/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+// readable stream
+
+// wriateable stream
+
+// pull source and push source
+
+// pipe: pipeThrough pipeTo
