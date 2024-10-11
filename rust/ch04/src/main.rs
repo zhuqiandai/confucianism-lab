28b428bbeb07d1c6fff4d5a76aa19bd79c73bf76
wangzhx <1132154046@qq.com>
2024-10-11 17:40:28 +0800

    rust

--- rust/ch04/src/main.rs
+++ rust/ch04/src/main.rs
@@ -74,8 +74,9 @@ fn main() {
 
     let mut v: Vec<i32> = vec![1, 2, 3];
     let num: &i32 = &v[2];
+
+    println!("{}", *num);
     v.push(4);
-    println!("Third element is {}", *num);
 
     // println!("boxed: {boxed_x}");
     // println!("unbox: {unbox_x}");
