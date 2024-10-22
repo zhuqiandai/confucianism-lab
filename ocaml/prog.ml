d144dbda154b8ff5eb64d081c3ce5e43ea50caf5
wangzhx <1132154046@qq.com>
2024-10-22 15:59:08 +0800

    ocaml

--- ocaml/prog.ml
+++ ocaml/prog.ml
@@ -1,9 +1,13 @@
-(* 分解 *)
 let rec digit_sum x = 
   if x < 10 then x
-else digit_sum (x / 10) + (x mod 10);;
+  else digit_sum (x / 10) + (x mod 10);;
 
 
-print_int (digit_sum 36)
+(* print_int (digit_sum 36) *)
 
-let rec sort (x, y, z) = 10
+
+let rec rem x y = if x < y then x else rem (x - y) y;;
+
+print_int (rem 10 3)
+
+let matrix = [|[|1; 2; 3|]; [|4; 5; 6|]|]
